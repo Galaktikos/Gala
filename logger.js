9220c88e94821e4044a91d27eb2ee7925f3e4723
Galaktikos <40399277+Galaktikos@users.noreply.github.com>
2019-05-08 08:13:26 -0400

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,5 +1,5 @@
 exports.run = function (message) {
-    var d = new Date(1382086394000);
+    var d = new Date(message.timestamp);
     
     console.log(d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + ' ' + message.author.username + ': ' message.content);
 }
