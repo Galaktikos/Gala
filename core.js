515e2bbd84dd50dc194aae4cd2c5bf7e9989c857
Galaktikos <40399277+Galaktikos@users.noreply.github.com>
2019-05-13 09:53:07 -0400

    Update core.js

--- core.js
+++ core.js
@@ -58,7 +58,7 @@ function message() {
 message();
 
 let commandCheck = {
-    'run' = function (message, client) { // Command
+    'run': function (message, client) { // Command
         if (message.content[1]) {
             fs.readdir('./Commands', function(err, items) { // Read commands directory
                 items = items.filter(function(ele){return ele != 'main.js'});
