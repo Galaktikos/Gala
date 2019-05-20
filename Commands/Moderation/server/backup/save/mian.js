d0c1be10954062d22433c88cc76239234746ef89
Galaktikos <40399277+Galaktikos@users.noreply.github.com>
2019-05-20 10:20:23 -0400

    Update mian.js

--- Commands/Moderation/server/backup/save/mian.js
+++ Commands/Moderation/server/backup/save/mian.js
@@ -2,8 +2,18 @@ const gala = require("../../../../Gala.js"), // Gala core
     fs = require("fs"); // File system
 
 exports.run = function (message, color) {
-    fs.readFile('./backups.json', 'utf8', function readFileCallback (err, data) { // Read file
-        
+    fs.readFile('./data.json', 'utf8', function readFileCallback (err, data) { // Read file
+        let server = {
+            'settings': {
+                'name': '';
+                
+            },
+            'categories': [
+                'uncategorized': {
+                    'channels': [ ]
+                }
+            ]
+        }
     }
 }
 
